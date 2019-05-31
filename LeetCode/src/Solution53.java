ca50e5b50b33bd189766422ce4130d758e1f2183
Yuan Fang <43736431+ponyo0818@users.noreply.github.com>
2019-05-31 17:22:25 -0400

    53

---
+++ LeetCode/src/Solution53.java
@@ -0,0 +1,26 @@
+/**
+ * 53. Maximum Subarray
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum
+ * and return its sum.
+ *
+ * Example:
+ *
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ */
+public class Solution53 {
+    public int maxSubArray(int[] A) {
+        int n = A.length;
+        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
+        dp[0] = A[0];
+        int max = dp[0];
+
+        for(int i = 1; i < n; i++){
+            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
